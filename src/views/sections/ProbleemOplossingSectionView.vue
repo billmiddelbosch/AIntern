748dd96039f1fcbb0b8cce4c3dbc7bf1d604407c
Bill <b.middelbosch@bm-c.nl>
2026-03-25 22:04:16 +0100

    Added probleem oplossingen sectie

---
+++ src/views/sections/ProbleemOplossingSectionView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import data from '@/../product/sections/problemen-oplossingen/data.json'
+import type { ProbleemOplossingPair } from '@/../product/sections/problemen-oplossingen/types'
+import { ProbleemOplossingSection } from '@/components/sections/problemen-oplossingen'
+
+const pairs = ref(data.pairs as ProbleemOplossingPair[])
+</script>
+
+<template>
+  <ProbleemOplossingSection :pairs="pairs" />
+</template>
