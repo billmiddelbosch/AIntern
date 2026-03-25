824814b35802fd5e4df0d3b331127c7f2c4b7f83
Bill <b.middelbosch@bm-c.nl>
2026-03-25 23:07:32 +0100

    Added FAQ section

---
+++ src/components/sections/no-cure-no-pay-faq/index.ts
@@ -0,0 +1,3 @@
+export { default as NoCureNoPayFaqSection } from './NoCureNoPayFaqSection.vue'
+export { default as NoCureNoPayProposition } from './NoCureNoPayProposition.vue'
+export { default as FaqAccordionItem } from './FaqAccordionItem.vue'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import HeroSectionView from '@/views/sections/HeroSectionView.vue'
 import HowItWorksSectionView from '@/views/sections/HowItWorksSectionView.vue'
 import ProbleemOplossingSectionView from '@/views/sections/ProbleemOplossingSectionView.vue'
 import ResultatenCasesSectionView from '@/views/sections/ResultatenCasesSectionView.vue'
+import NoCureNoPayFaqSectionView from '@/views/sections/NoCureNoPayFaqSectionView.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import ResultatenCasesSectionView from '@/views/sections/ResultatenCasesSectionV
     <HowItWorksSectionView />
     <ProbleemOplossingSectionView />
     <ResultatenCasesSectionView />
+    <NoCureNoPayFaqSectionView />
     <!-- Additional sections will be added here -->
   </AppShell>
 </template>
