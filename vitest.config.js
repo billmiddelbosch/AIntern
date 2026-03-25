9b44dddfded0d62920756a30b2bd237d9a3c45fd
Bill <b.middelbosch@bm-c.nl>
2026-03-25 20:57:21 +0100

    Fix build issues

---
+++ vitest.config.js
@@ -0,0 +1,13 @@
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
+export default mergeConfig(viteConfig, defineConfig({
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/test/setup.ts'],
+        include: ['src/**/*.{test,spec}.{ts,tsx}'],
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+}));
