f2335d4588a12d78ea7757a086ce8b649a2e1a6e
Bill <b.middelbosch@bm-c.nl>
2026-03-30 20:13:34 +0200

    Add robot.txt and llms.txt

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import generateSitemap from 'vite-ssg-sitemap'
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
   ],
+  ssgOptions: {
+    onFinished() {
+      generateSitemap({ hostname: 'https://aintern.nl' })
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
